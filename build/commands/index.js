f7dba9226b299153399a07ab542f92c647ef418d
Vitor Hariel <vhbarauna@gmail.com>
2019-08-08 22:41:26 -0300

    Docker files

---
+++ build/commands/index.js
@@ -0,0 +1,35 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});const runPing = msg => {
+  msg.reply('pong');
+};
+
+const runInvite = async msg => {
+  const invite = await msg.channel.createInvite().catch(console.log);
+  msg.reply(`${invite}`);
+};
+
+const commands = [
+  {
+    message: 'ping',
+    run: runPing,
+  },
+  {
+    message: 'invite',
+    run: runInvite,
+  },
+];
+
+ function listenMessage(msg) {
+  const prefix = process.env.BOT_COMMAND_PREFIX;
+  const { content } = msg;
+  let found = false;
+
+  if (content.startsWith(prefix) && content !== prefix) {
+    commands.forEach(command => {
+      if (content.substring(prefix.length) === command.message) {
+        found = true;
+        return command.run(msg);
+      }
+    });
+    if (!found) return msg.reply('Command not found!');
+  }
+} exports.default = listenMessage;
