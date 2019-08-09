074b7544d8dbf8942346d51082762cdc3ab8e75b
Vitor Hariel <vhbarauna@gmail.com>
2019-08-08 21:48:51 -0300

    First commands

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-require("dotenv").config();
-const Discord = require("discord.js");
+import 'dotenv/config';
+import Discord from 'discord.js';
+import listenMessage from './commands';
+
 const client = new Discord.Client();
 
-client.on("ready", () => {
+client.on('ready', () => {
+  client.user.setPresence({
+    status: 'online',
+    game: {
+      name: 'Being awesome!',
+    },
+  }),
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("Pong!");
-  }
+client.on('message', (msg) => {
+  listenMessage(msg, client);
 });
 
 client.login(process.env.BOT_TOKEN);
