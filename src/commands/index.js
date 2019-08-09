c891be23fb215456e2ca944dfd68eaad9aafc5fb
Vitor Hariel <vhbarauna@gmail.com>
2019-08-09 09:18:44 -0300

    Help command

--- src/commands/index.js
+++ src/commands/index.js
@@ -7,6 +7,26 @@ const runInvite = async msg => {
   msg.reply(`${invite}`);
 };
 
+const runHelp = async msg => {
+  await msg.author.createDM();
+  await msg.author.dmChannel.send(`
+  Hi ${msg.author.toString()}!\n\nAny time you need help, you can send me a message here! :smile:
+  `);
+};
+
+const dmHelp = async msg => {
+  if (!msg.author.bot) {
+    await msg.author.createDM();
+    await msg.author.dmChannel.send(
+      'We will delivery this message to one of your staff. Thank you!'
+    );
+    const channel = msg.client.channels.find(
+      channel => channel.name === process.env.SERVER_STAFF_HELP_CHANNEL
+    );
+    await channel.send(`Help from ${msg.author.toString()}:\n\n${msg.content}`);
+  }
+};
+
 const commands = [
   {
     message: 'ping',
@@ -16,6 +36,10 @@ const commands = [
     message: 'invite',
     run: runInvite,
   },
+  {
+    message: 'help',
+    run: runHelp,
+  },
 ];
 
 export default function listenMessage(msg) {
@@ -23,13 +47,17 @@ export default function listenMessage(msg) {
   const { content } = msg;
   let found = false;
 
-  if (content.startsWith(prefix) && content !== prefix) {
-    commands.forEach(command => {
-      if (content.substring(prefix.length) === command.message) {
-        found = true;
-        return command.run(msg);
-      }
-    });
-    if (!found) return msg.reply('Command not found!');
+  if (msg.channel.type !== 'dm') {
+    if (content.startsWith(prefix) && content !== prefix) {
+      commands.forEach(command => {
+        if (content.substring(prefix.length) === command.message) {
+          found = true;
+          return command.run(msg);
+        }
+      });
+      if (!found) return msg.reply('Command not found!');
+    }
+  } else {
+    return dmHelp(msg);
   }
 }
